58579f46025a6a80fab001bac51f02ca2f000e0f
fares8lefi <fareslefi4@gmail.com>
2025-11-17 13:39:41 +0100

    create user login function

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,5 +3,5 @@ var router = express.Router();
 const userController=require('../Controllers/userController')
 /* GET users listing. */
 router.post('/createUser',userController.createUser)
-
+router.post('/loginUser',userController.loginUser)
 module.exports = router;
