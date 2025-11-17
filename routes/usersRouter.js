97247e603a71da90e3c8c3fd9a6614a68a729e28
fares8lefi <fareslefi4@gmail.com>
2025-11-17 19:51:03 +0100

    use middleware for session/authentication handling

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+
 const userController=require('../Controllers/userController')
+const {requireAuthUser}= require("../middlewares/authMiddelwares");
 /* GET users listing. */
 router.post('/createUser',userController.createUser)
 router.post('/loginUser',userController.loginUser)
+router.get('/getConnectedUser',requireAuthUser,userController.getConnecedtUser)
 module.exports = router;
