7dd17570a61713e07c99f373200206a002685b3e
fares8lefi <fareslefi4@gmail.com>
2025-11-29 20:16:52 +0100

    create suppliers model

---
+++ models/suppliersModel.js
@@ -0,0 +1,42 @@
+
+const mongoose = require('mongoose');
+
+
+const supplierModel = new mongoose.Schema({
+    name :{
+        type : String,
+        required : true
+    },
+    code :{
+        type:number ,
+        unique : true
+    },
+    email :{
+        type : String ,
+        unique : true,
+        match: [
+                /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                "L'adresse e-mail doit être valide ex: exemple@domaine.com .",
+            ],
+    },
+    phone :{
+        type : number ,
+        unique : true , 
+        required : true ,
+    },
+    address :{
+        type : String
+    },
+    is_active:{
+        type :  Boolean,
+        default : true
+    },
+     created_at: {
+            type : Date,
+            default : Date.now,
+        },
+})
+
+const Suppliers = mongoose.model('Suppliers', supplierModel);
+
+module.exports = Suppliers;
