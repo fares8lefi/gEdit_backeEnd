449fe319b4736295289e99fcc943c7edfb1a92ca
fares8lefi <fareslefi4@gmail.com>
2025-11-29 20:56:59 +0100

    fix error for create catigorie function

--- routes/categorieRouter.js
+++ routes/categorieRouter.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const categorieController =require('../Controllers/categorieController')
 
-router.post('/createCategorie',categorieController.createCategorie)
+router.post('/createCategorie',categorieController.createcategory)
 router.get('/getAllCategories',categorieController.getAllCategories)
 router.get('/updateCategorie',categorieController.updateCategorie)
 router.delete('/deleteCategorie/:id',categorieController.deleteCategorie)
