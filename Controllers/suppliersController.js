e05d4b5000126b62d2af7d7730c29c24bd44cbbd
fares8lefi <fareslefi4@gmail.com>
2025-11-29 21:33:43 +0100

    create delete suppliers function

--- Controllers/suppliersController.js
+++ Controllers/suppliersController.js
@@ -10,7 +10,27 @@ module.exports.addSuppliers = async function (req, res) {
       phone,
       address,
     });
-    res.status(201).json({success:true,suppliers});
+    res.status(201).json({ success: true, suppliers });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
+module.exports.deleteSuppliers = async function (req, res) {
+  try {
+    const id = req.params.id;
+
+    const verifId = await supplierModel.find({ id });
+
+    if (!verifId || verifId.length === 0) {
+      res.status(400).json({ success: false, message: "suppliers not found " });
+    }
+    await supplierModel.findOneAndDelete(id);
+    res
+      .status(201)
+      .json({ success: true, message: "suppliers delete successflly" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
