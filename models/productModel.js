f3e75d01e2e0c0c4afd3ce4653f5387cef796f2b
fares8lefi <fareslefi4@gmail.com>
2025-12-06 17:55:33 +0100

    apply supplier and category references on Product schema

--- models/productModel.js
+++ models/productModel.js
@@ -17,24 +17,33 @@ const productSchema = new mongoose.Schema({
   },
   purchase_price: {
     type: Number,
-
     required: true,
   },
   selling_price: {
     type: Number,
-
     required: true,
   },
   unit: {
     type: Number,
-
     required: true,
   },
   stock_min: {
     type: Number,
-
     required: true,
   },
+  // Relation vers un fournisseur (one supplier)
+  supplier: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Suppliers',
+    required: false, 
+  },
+  // Relations vers une ou plusieurs catégories
+  categories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Categorie',
+    },
+  ],
 });
 
 const Product = mongoose.model("Product", productSchema);
