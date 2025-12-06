d5efd654c7ce7d69bdcc8d2ffb9c4d152f139c98
fares8lefi <fareslefi4@gmail.com>
2025-12-06 18:11:29 +0100

    create add product router

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,4 +3,6 @@ var router = express.Router();
 const productController =require('../Controllers/productController')
 
 
+router.post('/addProduct',productController.addProduct)
+
 module.exports = router;
