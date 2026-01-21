32345f5e3632c2ef0da000f549e1e1b1ec4240fd
fares8lefi <fareslefi4@gmail.com>
2026-01-21 20:17:34 +0100

    Default redirection in case of error

--- app.js
+++ app.js
@@ -1,64 +1,71 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const session = require("express-session");
-
-var usersRouter = require("./routes/usersRouter");
-var categorieRouter = require("./routes/categorieRouter");
-var suppliersRouter = require("./routes/suppliersRouter");
-var productRouter = require("./routes/productRouter");
-
 require("dotenv").config();
-var app = express();
+
+const usersRouter = require("./routes/usersRouter");
+const categorieRouter = require("./routes/categorieRouter");
+const suppliersRouter = require("./routes/suppliersRouter");
+const productRouter = require("./routes/productRouter");
+
 const { connectToDb } = require("./config/db");
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+
+const app = express();
+
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// session middleware: ensures `req.session` exists
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "default_session_secret",
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false },
+    cookie: { secure: false }, 
   })
 );
 
+
 app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use("/api/users", usersRouter);
 app.use("/api/categorie", categorieRouter);
 app.use("/api/supplier", suppliersRouter);
 app.use("/api/product", productRouter);
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.originalUrl,
+  });
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
 });
+
+
 const http = require("http");
 const server = http.createServer(app);
+
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, '0.0.0.0', () => {
-  connectToDb();
+server.listen(PORT, "0.0.0.0", async () => {
+  await connectToDb();
   console.log(`Server is running on port ${PORT}`);
-})
+});
+
 module.exports = app;
